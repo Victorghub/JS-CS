7f61bab0ae80e69a255b2cc21d10a17ad65b0bb4
Victor <onetworacks@gmail.com>
2024-11-02 02:50:41 +0000

    first ever javascript code written on my own

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ console.log("hello world" [10])
 console.log("12345".length)
 console.log("hello world".length)
 
+//
+
+let name = "Victor"
+let itsRaining = "True"
+let planet = "Earth"
+let temperature = "22"
+
+temperature = 23
+temperature = temperature + 1
+console.log(temperature)
+
+//Practice Question
+
+/*
+ * Converting Temperatures
+ *
+ * Celcius to Farenheit Formula:
+ *
+ *   F = C x 1.8 + 32
+ */
+
+let Celcius = 10;
+let Farenheit =(Celcius * 1.8 + 32);
+
+console.log(Farenheit)
