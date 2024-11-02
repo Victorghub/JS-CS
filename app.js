7e714616e425819db1cee729a57a2ec7771f661e
Victor <onetworacks@gmail.com>
2024-11-02 00:47:35 +0000

    JV

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+console.log("hello world" + "second string")
+console.log("anything inside these squiggly lines is considered a string - even numbers. eg: 100")
+
+console.log("Victor"[0])
+console.log("hello world" [10])
+
+console.log("12345".length)
+console.log("hello world".length)
+
