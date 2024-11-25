e6dd5c1db47e9ce933045ea59d9bf0de2cd44975
Victor <onetworacks@gmail.com>
2024-11-25 18:19:18 +0000

    ARRAS

--- app.js
+++ app.js
@@ -250,13 +250,13 @@ console.log(convertCelciusToFahrenheit(0))
 
 //ARRAYS//
 
-let item1 = 20
-let item2 = 30
-let item3 = 40
-let item4 = 50
-let item5 = 100
+//let item1 = 20
+//let item2 = 30
+//let item3 = 40
+//let item4 = 50
+//let item5 = 100
 
-let arr = [20, 30, 40, 50, 100] //the numbers are all elements
+//let arr = [20, 30, 40, 50, 100] //the numbers are all elements
 
 //first element of array
 //console.log(arr[0])
@@ -266,14 +266,39 @@ let arr = [20, 30, 40, 50, 100] //the numbers are all elements
 
 //arr.push(200)
 
-let newArr = arr.filter((element) => {
-    console.log(element)
-    return true;
-})    //elemnt that we are currently on in the iteration
+//let newArr = arr.filter((element) => {
+  //  console.log(element)
+  //  return true;/
+//})    //elemnt that we are currently on in the iteration
 
-console.log(newArr)
+//console.log(newArr)
 
 //console.log(arr);
 
 // this gives and empty array because .filter method only returns the element of the
 //current itiarion we are on if that iteration turns true
+
+
+//array excersice//
+
+/**
+ * Filter out all the fail elements in an array
+ * 
+ * @examples
+ * [A+, A, FAIL] => [A+, A]
+ * [FAIL, AIL, B] => [B]
+ * [FAIL] => []
+ * 
+*/
+let grades = ['A+', 'A', 'FAIL']
+
+let goodGrades = grades.filter((element) => {
+    console.log(element)
+    if (element !== 'FAIL'){
+        return true;
+    }
+})
+
+console.log(goodGrades)
+
+
